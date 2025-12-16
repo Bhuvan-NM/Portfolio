64ab1b591a5a49faee771761c5fef006ae597a6d
Bhuvan-NM <bhuvanarasimha29@gmail.com>
2025-12-16 21:03:03 +1100

    initialised project and added nessecary configs and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./css/style.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <h1>Hello, Portfolio!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
