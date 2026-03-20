b89001fba64d09384eb5f4f1aa0f85e17bf9a14b
Bhuvan-NM <bhuvanarasimha29@gmail.com>
2026-03-20 18:22:09 +1100

    fixed all UI bugs in both contact page and the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -159,71 +159,73 @@ const Home = ({ onOpenContact }: HomeProps) => {
         </div>
       </InfoCard>
 
-      <div className="technicalSkills-Section">
-        <h2 className="technicalSkills-Heading">
-          <span>
-            <WrenchIcon className="wrench-icon" />
-          </span>
-          Skills and Interests
-        </h2>
-        <div className="technicalSkills-card-holder">
-          <InfoCard className="technicalSkills-card technical">
-            <h3>
-              <span>
-                <CodeIcon className="technicalSkills-card-HeadingIcon" />
-              </span>
-              Technical
-            </h3>
-            <div className="technicalSkills-row">
-              {TechnicalSkills.map((TechincalSkills) => (
-                <div
-                  key={TechincalSkills.name}
-                  className="technicalSkills-card-element"
-                >
-                  {TechincalSkills.icon}
-                  <span>{TechincalSkills.name}</span>
-                </div>
-              ))}
-            </div>
-          </InfoCard>
-          <InfoCard className="technicalSkills-card softSkills">
-            <h3>
-              <span>
-                <SkillsIcon className="technicalSkills-card-HeadingIcon" />
-              </span>
-              Soft Skills
-            </h3>
-            <div className="technicalSkills-row">
-              {SoftSkills.map((skill) => (
-                <div
-                  key={skill.name}
-                  className="technicalSkills-card-element"
-                >
-                  <span>{skill.name}</span>
-                </div>
-              ))}
-            </div>
-          </InfoCard>
-          <InfoCard className="technicalSkills-card interests">
-            <h3>
-              <span>
-                <LightBulbIcon className="technicalSkills-card-HeadingIcon" />
-              </span>
-              Interests
-            </h3>
-            <div className="technicalSkills-row">
-              {Interests.map((interest) => (
-                <div
-                  key={interest.name}
-                  className="technicalSkills-card-element"
-                >
-                  <span>{interest.name}</span>
-                </div>
-              ))}
-            </div>
-          </InfoCard>
+      <InfoCard className="technicalSkills-Section">
+        <div className="technicalSkills-Section">
+          <h2 className="technicalSkills-Heading">
+            <span>
+              <WrenchIcon className="wrench-icon" />
+            </span>
+            Skills and Interests
+          </h2>
+          <div className="technicalSkills-card-holder">
+            <InfoCard className="technicalSkills-card technical">
+              <h3>
+                <span>
+                  <CodeIcon className="technicalSkills-card-HeadingIcon" />
+                </span>
+                Technical
+              </h3>
+              <div className="technicalSkills-row">
+                {TechnicalSkills.map((TechincalSkills) => (
+                  <div
+                    key={TechincalSkills.name}
+                    className="technicalSkills-card-element"
+                  >
+                    {TechincalSkills.icon}
+                    <span>{TechincalSkills.name}</span>
+                  </div>
+                ))}
+              </div>
+            </InfoCard>
+            <InfoCard className="technicalSkills-card softSkills">
+              <h3>
+                <span>
+                  <SkillsIcon className="technicalSkills-card-HeadingIcon" />
+                </span>
+                Soft Skills
+              </h3>
+              <div className="technicalSkills-row">
+                {SoftSkills.map((skill) => (
+                  <div
+                    key={skill.name}
+                    className="technicalSkills-card-element"
+                  >
+                    <span>{skill.name}</span>
+                  </div>
+                ))}
+              </div>
+            </InfoCard>
+            <InfoCard className="technicalSkills-card interests">
+              <h3>
+                <span>
+                  <LightBulbIcon className="technicalSkills-card-HeadingIcon" />
+                </span>
+                Interests
+              </h3>
+              <div className="technicalSkills-row">
+                {Interests.map((interest) => (
+                  <div
+                    key={interest.name}
+                    className="technicalSkills-card-element"
+                  >
+                    <span>{interest.name}</span>
+                  </div>
+                ))}
+              </div>
+            </InfoCard>
+          </div>
         </div>
-      </div>
+      </InfoCard>
 
       <div className="img-wrap desktop-only">
         <img
